3fc74c7c8e489c8900cedc7362dfbd906231a631
hbashdevv <105935953+hbashdevv@users.noreply.github.com>
2022-05-29 14:19:41 +0300

    Update youtube.py

--- VCsMusicBot/services/downloaders/youtube.py
+++ VCsMusicBot/services/downloaders/youtube.py
@@ -21,14 +21,14 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
-            f"the provided video is {duration} minute(s)",
+            f"🛑 لايمكنك تنزيل فيديوهات التي تزيد مدتها {DURATION_LIMIT} دقيقه, "
+            f"الفيديو المقدم هو {duration} دقيقه",
         )
     try:
         ydl.download([url])
     except:
         raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
+            f"🛑 لايمكنك تنزيل فيديوهات التي تزيد مدتها {DURATION_LIMIT} دقيقه, "
             f"the provided video is {duration} minute(s)",
         )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
